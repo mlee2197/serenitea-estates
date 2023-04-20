2d887cb5220c4a0c257928032532e5b3f1ad96e0
Matt <mlee138@binghamton.edu>
2023-04-20 18:57:24 -0400

    add fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { Dosis, Inter } from "next/font/google";
+
+const inter = Inter({
+  weight: ["300"],
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const dosis = Dosis({
+  weight: ["600"],
+  variable: "--font-dosis",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider>
-      <Component {...pageProps} />
+      <main className={`${inter.variable} ${dosis.variable}`}>
+        <Component {...pageProps} />
+      </main>
     </SessionProvider>
   );
 }
